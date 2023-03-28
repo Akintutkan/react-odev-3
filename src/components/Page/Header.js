f999b35bcac66c363cef7d0341323f7711b36865
Akin94 <akin.tutkan@gmail.com>
2023-03-29 02:40:48 +0300

    ..

--- src/components/Page/Header.js
+++ src/components/Page/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../../style/header.css";
+import "../../style/header.css";
 import SettingsBtn from "../IconComponent/SettingsBtn";
 import locationGif from "../../assets/icons/location.gif";
 import useGeoLocation from "../../hook/useGeoLocation";
